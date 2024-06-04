ef2d25cc13c7f71cb4169a59091993742c943336
Victoria1877 <victoriab1877@gmail.com>
2024-06-04 15:33:41 +0930

    step 4 complete
    Added Date Time

--- Assessment 2/myScripts.js
+++ Assessment 2/myScripts.js
@@ -1,9 +1,17 @@
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Benjamin Bowering // // 04-06-2024 // myScripts.js// // This is my own work as defined by the /////////////
+ // university's academic integrity policy. ///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 function showLogo(p1, img1){
     document.getElementById('img1').src = "https://pbs.twimg.com/profile_images/1366305040420380673/C5dVrQWd_400x400.jpg";
+    document.getElementById('img1').style = "border: 15px solid orange;"
     document.getElementById('p1').innerHTML = "An image of the logo of UniSA where i attend. Available at:<a href='https://www.facebook.com/UniSA/'>https://www.facebook.com/UniSA/</a>";
 }
 function showCampus(p1, img1){
     document.getElementById('img1').src = "https://www.saibt.sa.edu.au/content/dam/navitas/upa/saibt/images/web/spotlight-3-col/unisa-mawson-lakes-campus.jpg";
+    document.getElementById('img1').style = "border: 15px solid orange;"
     document.getElementById('p1').innerHTML = "An image of the campus i attend. Available at: <a href='https://www.saibt.sa.edu.au/about/unisa/city-mawson-lakes-campus'>https://www.saibt.sa.edu.au/about/unisa/city-mawson-lakes-campus</a>";
 }
 function toggleThatTheme(){
@@ -11,9 +19,17 @@ function toggleThatTheme(){
     var desiredTheme = "light-mode";
 
     if (activeTheme === "light-mode") {
-        desiredTheme = "dark-mode";
-    }
+        desiredTheme = "dark-mode";}
+
+    if (activeTheme == null){
+        desiredTheme = "dark-mode"}
 
     document.documentElement.setAttribute('data-theme', desiredTheme)
     localStorage.setItem('theme', desiredTheme);
 }
+function getDateTime(){
+    const theDateTime = new Date();
+    const currentDateTime = theDateTime.toLocaleString();
+    document.getElementById("dateTime").innerHTML = currentDateTime
+}
+setInterval(getDateTime, 1000);
